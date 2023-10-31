2b82466603d0e822af5f6ec4e28fb0298be1734c
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2023-10-31 13:05:15 +0000

    Reformat code

--- test/nextauth.d.ts
+++ test/nextauth.d.ts
@@ -1,17 +1,17 @@
 // Read more at: https://next-auth.js.org/getting-started/typescript#module-augmentation
-import { DefaultSession } from "next-auth";
+import { DefaultSession } from 'next-auth';
 
-declare module "next-auth" {
+declare module 'next-auth' {
   interface User {
     accessToken: string;
     refreshToken: string;
   }
   interface Session {
-    user: DefaultSession["user"] & import("../src").SubriteProfile;
+    user: DefaultSession['user'] & import('../src').SubriteProfile;
   }
 }
 
-declare module "next-auth/jwt" {
+declare module 'next-auth/jwt' {
   interface JWT {
     accessToken: string;
   }
